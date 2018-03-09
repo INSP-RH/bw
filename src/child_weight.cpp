c1f3015dd5a1a6a124131cfdc1125c5039148a98
Dalia Camacho García Formentí <daliaf172@gmail.com>
2018-03-09 16:09:41 -0600

    Update child_weight.cpp

--- src/child_weight.cpp
+++ src/child_weight.cpp
@@ -9,10 +9,7 @@
 #include "child_weight.h"
 #include <stdio.h>     
 #include <math.h>
-#include <vector>
 #include <iostream>   
-#include <algorithm>  
-using namespace std;
 
 Child::Child(NumericVector input_age, NumericVector input_sex, NumericVector input_FFM, NumericVector input_FM, NumericMatrix input_EIntake, bool checkValues){
     age   = input_age;
