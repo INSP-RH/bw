7a7e66ba7016b145fdacded16a4b16f5b89b13eb
Dalia Camacho García Formentí <daliaf172@gmail.com>
2018-03-12 16:02:08 -0600

    Update child_weight.cpp

--- src/child_weight.cpp
+++ src/child_weight.cpp
@@ -356,7 +356,7 @@ void Child::getParameters(void){
 
 //Intake in calories
 NumericVector Child::Intake(NumericVector t){
-    double timeval = (t(0) - age(0))*365;
+    double timeval = std::min((t(0) - age(0))*365),days);
     return EIntake(floor(timeval),_);
 }
 
