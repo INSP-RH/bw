f3deb0e68720f34480c291337bbe3fc7a153a681
Dalia Camacho García Formentí <daliaf172@gmail.com>
2018-03-12 17:36:32 -0600

    Update child_weight_wrapper.cpp

--- src/child_weight_wrapper.cpp
+++ src/child_weight_wrapper.cpp
@@ -46,10 +46,10 @@ NumericMatrix intake_reference_wrapper(NumericVector age, NumericVector sex, Num
     Child Person (age,  sex, FFM, FM, EI, false);
     
     //Energy matrix
-    NumericMatrix EnergyIntake(age.size(), (days+365));
+    NumericMatrix EnergyIntake(age.size(), (days));
     
     //Get energy matrix
-    for (double i = 0; i <= (days+365); i++){
+    for (double i = 0; i <= (days); i++){
         EnergyIntake(_,i) = Person.IntakeReference(age + i/365.0);
     }
     
