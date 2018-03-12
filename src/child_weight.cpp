d9767a0447c5776912aa45c738f4b15279ee07d3
Dalia Camacho García Formentí <daliaf172@gmail.com>
2018-03-12 16:55:56 -0600

    Update child_weight.cpp

--- src/child_weight.cpp
+++ src/child_weight.cpp
@@ -309,7 +309,7 @@ void Child::getParameters(void){
     //General constants
     rhoFM    = 9.4*1000;
     deltamin = 10.0;
-    P        = 12.0*365;
+    P        = 12.0;
     h        = 10.0;
     
     //Number of individuals
@@ -325,30 +325,30 @@ void Child::getParameters(void){
     A         = 3.2*(1 - sex) + 2.3*sex;
     B         = 9.6*(1 - sex) + 8.4*sex;
     D         = 10.1*(1 - sex) + 1.1*sex;
-    tA        = 4.7*(1 - sex)*365 + 4.5*sex*365;
-    tB        = 12.5*(1 - sex)*365 + 11.7*sex*365;
-    tD        = 15.0*(1-sex)*365 + 16.2*sex*365;
-    tauA      = 2.5*(1 - sex)*365 + 1.0*sex*365;
-    tauB      = 1.0*(1 - sex)*365 + 0.9*sex*365;
-    tauD      = 1.5*(1 - sex)*365 + 0.7*sex*365;
+    tA        = 4.7*(1 - sex) + 4.5*sex;
+    tB        = 12.5*(1 - sex) + 11.7*sex;
+    tD        = 15.0*(1-sex) + 16.2*sex;
+    tauA      = 2.5*(1 - sex) + 1.0*sex;
+    tauB      = 1.0*(1 - sex) + 0.9*sex;
+    tauD      = 1.5*(1 - sex) + 0.7*sex;
     A_EB      = 7.2*(1 - sex) + 16.5*sex;
     B_EB      = 30*(1 - sex) + 47.0*sex;
     D_EB      = 21*(1 - sex) + 41.0*sex;
-    tA_EB     = 5.6*(1 - sex)*365 + 4.8*sex*365;
-    tB_EB     = 9.8*(1 - sex)*365 + 9.1*sex*365;
-    tD_EB     = 15.0*(1 - sex)*365 + 13.5*sex*365;
-    tauA_EB   = 15*(1 - sex)*365 + 7.0*sex*365;
-    tauB_EB   = 1.5*(1 -sex)*365 + 1.0*sex*365;
-    tauD_EB   = 2.0*(1 - sex)*365 + 1.5*sex*365;
+    tA_EB     = 5.6*(1 - sex) + 4.8*sex;
+    tB_EB     = 9.8*(1 - sex) + 9.1*sex;
+    tD_EB     = 15.0*(1 - sex) + 13.5*sex;
+    tauA_EB   = 15*(1 - sex) + 7.0*sex;
+    tauB_EB   = 1.5*(1 -sex) + 1.0*sex;
+    tauD_EB   = 2.0*(1 - sex) + 1.5*sex;
     A1        = 3.2*(1 - sex) + 2.3*sex;
     B1        = 9.6*(1 - sex) + 8.4*sex;
     D1        = 10.0*(1 - sex) + 1.1*sex;
-    tA1       = 4.7*(1 - sex)*365 + 4.5*sex*365;
-    tB1       = 12.5*(1 - sex)*365 + 11.7*sex*365;
-    tD1       = 15.0*(1 - sex)*365 + 16.0*sex*365;
-    tauA1     = 1.0*(1 - sex)*365 + 1.0*sex*365;
-    tauB1     = 0.94*(1 - sex)*365 + 0.94*sex*365;
-    tauD1     = 0.69*(1 - sex)*365 + 0.69*sex*365;
+    tA1       = 4.7*(1 - sex) + 4.5*sex;
+    tB1       = 12.5*(1 - sex) + 11.7*sex;
+    tD1       = 15.0*(1 - sex) + 16.0*sex;
+    tauA1     = 1.0*(1 - sex) + 1.0*sex;
+    tauB1     = 0.94*(1 - sex) + 0.94*sex;
+    tauD1     = 0.69*(1 - sex) + 0.69*sex;
     
    
 }
